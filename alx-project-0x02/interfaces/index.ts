8808063f27a2c303a9459adca84cc13781b405fb
Leul Zewdu <Leulzewduchanie@gmail.com>
2025-06-07 18:46:04 +0300

    added interface button.tsx

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,29 @@
 export interface CardProps{
     title: string;
-  content: string;
+    content: string;
+}
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (title: string, content: string) => void;
+}
+export interface ButtonProps {
+  size?: 'small' | 'medium' | 'large';
+  shape?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  onClick?: () => void;
+}
+export interface PostProps {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+  };
 }
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,13 +1,19 @@
-// pages/about.tsx
-import Header from '@/components/layout/Header'
+import React from 'react';
+import Button from '@/components/common/Button';
+import Header from '@/components/layout/Header';
 
-export default function AboutPage() {
+const AboutPage = () => {
   return (
-    <>
-      <Header />
-      <main className="p-6">
-        <h1 className="text-2xl font-bold">About Page</h1>
-      </main>
-    </>
-  )
-}
+    <div className="p-8">
+        <Header/>
+      <h1 className="text-2xl font-bold mb-6">About Page</h1>
+      <div className="flex gap-4 flex-wrap">
+        <Button size="small" shape="rounded-sm" />
+        <Button size="medium" shape="rounded-md" />
+        <Button size="large" shape="rounded-full" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
